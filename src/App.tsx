1ee07a752fa7d24f1e05d69b0ff926a5b456b8fd
Damian <damianmirek1234@gmail.com>
2023-08-05 16:54:35 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+    return (
+        <div className="rounded py-2 text-base text-white bg-blue-500 px-4">
+            <span className="py-2 bg-blue-500 px-4">asd2</span>
+        </div>
+    );
+}
+
+export default App;
