313988a1257cf470dd4734b387545405931890ff
Damian <damianmirek1234@gmail.com>
2023-08-08 19:54:46 +0200

    Basic styling applied

---
+++ src/App/Common/ListOfProducts/index.tsx
@@ -0,0 +1,34 @@
+import { Product } from "../Product";
+import LeftArrow from "../../../graphics/Common/left_arrow.svg";
+import RightArrow from "../../../graphics/Common/right_arrow.svg";
+import { ProductApi } from "../../../api/types";
+import classes from "../styles/ListOfProducts.module.css";
+
+export const ListOfProducts = ({ products }: { products: ProductApi[] }) => {
+    const {
+        productsListWrapper,
+        navigationLeftArrow,
+        navigationRightArrow,
+        productItemsWrapper,
+    } = classes;
+
+    return (
+        <div className={productsListWrapper}>
+            <img
+                src={LeftArrow}
+                alt="left arrow"
+                className={navigationLeftArrow}
+            />
+            <div className={productItemsWrapper}>
+                {products.map((product) => (
+                    <Product {...product} />
+                ))}
+            </div>
+            <img
+                src={RightArrow}
+                alt="right arrow"
+                className={navigationRightArrow}
+            />
+        </div>
+    );
+};
