84d1b8fc362b3abc3ea1ce12b76dbbcdb871d645
Damian <damianmirek1234@gmail.com>
2023-08-10 19:55:49 +0200

    New product styles applied

--- src/App/Common/Product/index.tsx
+++ src/App/Common/Product/index.tsx
@@ -1,21 +1,18 @@
 import { ProductApi } from "../../../api/types";
 import classes from "../styles/Product.module.css";
+import { formatMoney } from "../../../utils/formatters";
 
-export const Product = ({ title, price, description, images }: ProductApi) => {
-    const {
-        productTitle,
-        productPrice,
-        productDescription,
-        productImage,
-        productGridContainer,
-    } = classes;
+export const Product = ({ title, price, images }: ProductApi) => {
+    const { productTitle, productPrice, productImage, productWrapper } =
+        classes;
 
     return (
-            <div className={productGridContainer}>
-                <img src={images[0]} alt={title} className={productImage} />
-                <h3 className={productTitle}>{title}</h3>
-                <span className={productPrice}>{price}</span>
-                <p className={productDescription}>{description}</p>
-            </div>
+        <div className={productWrapper}>
+            <img src={images[0]} alt={title} className={productImage} />
+            <h3 className={productTitle}>{title}</h3>
+            <span className={productPrice}>
+                {formatMoney({ amount: price, currency: "PLN" })}
+            </span>
+        </div>
     );
 };
