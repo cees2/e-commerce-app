6a7a4bd4cf5cad71475670deea6cbba745beb5c8
Damian <damianmirek1234@gmail.com>
2023-09-16 08:27:27 +0200

    New icon in sticky bar

--- src/App/Layout/pages/Header/pages/StickyNavigation/index.tsx
+++ src/App/Layout/pages/Header/pages/StickyNavigation/index.tsx
@@ -2,6 +2,7 @@ import classes from "../../styles/StickyNavigation.module.css";
 import HomeSVG from "../../../../../../graphics/StickyNavigation/home.svg";
 import ShoppingCart from "../../../../../../graphics/StickyNavigation/shopping_cart.svg";
 import UserAccount from "../../../../../../graphics/StickyNavigation/user_account.svg";
+import LogIn from "../../../../../../graphics/StickyNavigation/log_in.svg";
 import { Link } from "react-router-dom";
 
 export const StickyNavigation = () => {
@@ -21,12 +22,20 @@ export const StickyNavigation = () => {
                     <p>Cart</p>
                 </div>
             </Link>
-            <Link to="/user">
+            <Link to="/authentication/login">
                 <div className={stickyNavigationItem}>
-                    <img src={UserAccount} alt="user account" />
-                    <p>Account</p>
+                    <img src={LogIn} alt="log in" />
+                    <p>Log in</p>
                 </div>
             </Link>
+            {false && (
+                <Link to="/user">
+                    <div className={stickyNavigationItem}>
+                        <img src={UserAccount} alt="user account" />
+                        <p>Account</p>
+                    </div>
+                </Link>
+            )}
         </div>
     );
 };
