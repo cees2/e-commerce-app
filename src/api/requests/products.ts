2609690a03e14248a19cfb76c19ea0cc102d0b1c
Damian <damianmirek1234@gmail.com>
2023-10-23 20:01:18 +0200

    loginUser method bug fixed & request function invoke in submit handler

--- src/api/requests/products.ts
+++ src/api/requests/products.ts
@@ -2,6 +2,13 @@ import axios from "axios";
 import { NewProductApi } from "../../App/Layout/pages/Products/services/types";
 import { BASE_URL } from "../common";
 
-export const createProduct = (data: NewProductApi): Promise<NewProductApi> => {
-    return axios.post(`${BASE_URL}/products`, data);
+export const createProduct = (
+    data: NewProductApi,
+    token: string | null,
+): Promise<NewProductApi> => {
+    return axios.post(`${BASE_URL}/products`, data, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
 };
